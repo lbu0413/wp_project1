e38623dbfe681bebd9b6182242ce5d4a70870388
jamorr <Morris.Jack@outlook.com>
2023-07-03 09:15:52 -0400

    added ordinal func and fixed r/w/u scores

--- Project1/common.php
+++ Project1/common.php
@@ -6,17 +6,52 @@
  */
 function check_auth()
 {
-    $protocol = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ?
-        'https://' : 'http://';
-    $host = $_SERVER['HTTP_HOST'];
-    $currentURL = $protocol . $host;
-
     // redirect to login page if no authentication from login
     if (!isset($_SESSION['auth']) || !$_SESSION['auth']) {
         header("Location: signin.html");
-    } else {
-        return $currentURL;
+    } 
+}
+/**
+ * Get the ordinal suffix for integer value 
+ *
+ * @param  number integer to get suffix for 
+ * @return string suffix 
+ */
+function ordinal(int $number):string
+{
+    return match($number) {
+        1 => "1st",
+        2 => "2nd",
+        3 => "3rd",
+        default => $number."th"
+    };
+}
+/** 
+ * Convert array of strings into associative array of arrays 
+ * where each value is a username score pair
+ *
+ * @param  data array of strings 
+ * @return array associative array of arrays of strings 
+ */
+function read_into_associative(array $data):array
+{
+
+    $out = array();
+    $values = array();
+    $keys = array();
+    // reads alternating keyval array of strings into assoc array
+    for ($i = 0; $i < count($data); $i+=2) {
+        array_push($keys, $data[$i]);
     }
+    for ($i = 1; $i < count($data); $i+=2) {
+        array_push($values, $data[$i]);
+    }
+    for ($i = 0; $i < count($values); $i++) {
+        $out[$i] = array($keys[$i], $values[$i]);
+    }
+    unset($keys, $values);
+    $_SESSION['leaderboard'] = $out;
+    return $out;
 }
 
 /** 
@@ -26,49 +61,64 @@ function check_auth()
  */
 function read_leaderboard():array
 {
-    $scores = [];
-    $name = '';
-    $fptr = fopen("./user_data/scores.txt", 'r')or die("Unable to open file!");
-    for ($i = 0; ! feof($fptr); $i++){
-        if ($i%2 === 0) {
-            $name = fgets($fptr);
-        }else {
-            $scores += [$name, (int)fgets($fptr)];
-        }
+    if (!file_exists("./user_data/scores.txt")) {
+        touch("./user_data/scores.txt");
     }
-  
-    fclose($fptr);
-    return $scores;
+    $fstr = explode("\n", file_get_contents("./user_data/scores.txt"));
+    if (!$fstr[count($fstr)-1]) {
+        array_pop($fstr);
+    }
+    return read_into_associative($fstr);
   
 }
-function write_leaderboard(array $leaderboard)
+/**
+ * Convert associative array into string and write to score file 
+ *
+ * @param  leaderboard associative array with array values 
+ * @return void 
+ */
+function write_leaderboard(array $leaderboard):void
 {
-    $fptr = fopen("./user_data/scores.txt", 'w')or die("Unable to open file!");
-    for ($i = 0;! feof($fptr);$i++) {
-        fwrite($fptr, $leaderboard[$i][0].'\n'.$leaderboard[$i][1].'\n');
+    if (!file_exists("./user_data/scores.txt")) {
+        touch("./user_data/scores.txt");
+    }  
+    $leader_string = "";
+    foreach ($leaderboard as $data) {
+        $leader_string .= implode("\n", $data)."\n";
     }
-    fclose($fptr);
+    // prevent others from r/w while writing
+    file_put_contents("./user_data/scores.txt", $leader_string, LOCK_EX);
 }
-function update_leaderboard($new_score)
+/**
+ * Update leaderboard with session score 
+ *
+ * @return int position of score in updated leaderboard (1 indexed)
+ */
+function update_leaderboard():int
 {
     $leaderboard = read_leaderboard();
+    $new_hs = 0;
     if (! count($leaderboard)) {
-        $leaderboard += [$_SESSION['username'], $new_score];
-        return $leaderboard;
-    }
-    for ($i = count($leaderboard)-1; $i >= 0 ; $i--) {
-        // insert value into leaderboard if its deserving
-        if (!isset($leaderboard[$i]) || $leaderboard[$i][1] < $new_score) {
-            array_splice($leaderboard, $i, 0, [$_SESSION['username'], $new_score]);
-            break;
+        array_push($leaderboard, array($_SESSION['username'], $_SESSION['score']));
+        $new_hs++;
+    } else {
+        for ($i = 0; $i < 10 ; $i++) {
+            // add to leaderboard if slot is empty or less than score
+            if (! $leaderboard[$i] || (float)$leaderboard[$i][1] < $_SESSION['score']) {
+                array_splice($leaderboard, $i, 0, [ [$_SESSION['username'], $_SESSION['score']] ]);
+                $new_hs = $i+1;
+                break;
+            }
+        }
+        // only keep top 10 in leaderboard
+        while (count($leaderboard) > 10) {
+            array_pop($leaderboard);
         }
     }
-    // only keep top 10 in leaderboard
-    while (count($leaderboard) > 10) {
-        array_pop($leaderboard);
-    }
   
+    
     write_leaderboard($leaderboard);
+    return $new_hs;
 }
 
 ?>
