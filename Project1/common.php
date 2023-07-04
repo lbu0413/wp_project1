7cb82be54d82fb3e1e3f7d25f3ea1601b27b3a6d
lbu0413 <vvookly@gmail.com>
2023-07-04 19:46:00 -0400

    register, signin page logic updated

--- Project1/common.php
+++ Project1/common.php
@@ -1,21 +1,19 @@
 <?php
 function check_auth()
 {
-    $protocol = ! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ?
-    'https://' : 'http://';
+    $protocol = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ?
+        'https://' : 'http://';
     $host = $_SERVER['HTTP_HOST'];
-    $currentURL = $protocol.$host;
+    $currentURL = $protocol . $host;
 
     // redirect to login page if no authentication from login
-    if (! isset($_SESSION['auth']) || !$_SESSION['auth'] ) {
+    if (!isset($_SESSION['auth']) || !$_SESSION['auth']) {
         header("Location: $currentURL/signin.php");
     } else {
         return $currentURL;
     }
 
 }
-
-
 
 
 ?>
